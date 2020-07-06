8acf4de99def079ee1a44d980e9cf891881adcf4
arifhossain123 <arifhossain42136@gmail.com>
2020-07-06 23:38:14 +0600

    Updates

---
+++ precache-manifest.158924587f8d0323db576df69bce6b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cb15621607fa71432cac59a5c3b574",
+    "url": "/react-HOC/index.html"
+  },
+  {
+    "revision": "de53f9c326626ff5145c",
+    "url": "/react-HOC/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8af1e9744a3effd5cea4",
+    "url": "/react-HOC/static/js/2.c9a59fcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-HOC/static/js/2.c9a59fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de53f9c326626ff5145c",
+    "url": "/react-HOC/static/js/main.990743e7.chunk.js"
+  },
+  {
+    "revision": "ba16d0b5789b4c4d8104",
+    "url": "/react-HOC/static/js/runtime-main.5610be21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-HOC/static/media/logo.5d5d9eef.svg"
+  }
+]);
